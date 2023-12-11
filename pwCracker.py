12287b5c3c5c5d6c1f30de7c9675027f9a9207cd
ICLimon <149421819+ICLimon@users.noreply.github.com>
2023-12-11 10:54:53 -0600

    Update pwCracker.py

--- pwCracker.py
+++ pwCracker.py
@@ -4,6 +4,7 @@ import string
 
 pwGuess = ""
 guess = ""
+attempts = 0
 passwordFile = "passList.txt"
 
 attack = input("Select attack: Dictionary  |  Brute     ")
@@ -16,12 +17,12 @@ def md5Password(password):
     encodedMd5Password = password.encode("utf-8")
     return(hashlib.md5(encodedMd5Password.rstrip()).hexdigest())
 
-# Trandlates password to SHA-256 hash
+# Translates password to SHA-256 hash
 def shaPassword(password):
     encodedSha256Password = password.encode("utf-8")
     return(hashlib.sha256(encodedSha256Password.rstrip()).hexdigest())
 
-# Gets random word
+# Applies brute force attack
 def bruteForce():
         guess = ''.join(random.choices(string.ascii_letters + string.digits, k = len(password)))
         return guess
@@ -34,10 +35,10 @@ if(attack == "Dictionary"):
     for word in passwordFile:
         if(mode == "MD5"):
             guess = md5Password(word)
-            pwGuess = md5Password(password)
+            pwGuess = password.rstrip()
         elif(mode == "SHA-256"):
             guess = shaPassword(word)
-            pwGuess = shaPassword(password)
+            pwGuess = password.rstrip()
         elif(mode == "Text"):
             guess = word.rstrip()
             pwGuess = password.rstrip()
@@ -47,7 +48,8 @@ if(attack == "Dictionary"):
             print("\n")
             print(f"Attempts: {count}")
             print("\n")
-            print("Password: " + pwGuess.rstrip())
+            print("Password: " + pwGuess.rstrip() )
+            print("-----" + str(word) + " -----")
             quit()
     print("Password not found")
 
@@ -57,21 +59,24 @@ if(attack == "Brute"):
     while True:
         randomWord = bruteForce()
         if(mode == "MD5"):
-            pwGuess = md5Password(password)
+            pwGuess = password.rstrip()
             guess = md5Password(randomWord)
         elif(mode == "SHA-256"):
-            pwGuess = shaPassword(password)
+            pwGuess = password.rstrip()
             guess = shaPassword(randomWord)
         elif(mode == "Text"):
             pwGuess = password.rstrip()
             guess = randomWord.rstrip()
             
-        count = count + 1
+        attempts = attempts + 1
+        print(attempts)
         print("-----" + str(randomWord) + "-----")
+
+        # Limit so my computer doesn't explode
         if(attempts > 100000):
             print("Password not found")
             quit()
         if(guess == pwGuess):
-            print(f"Attempts : {count}")
+            print(f"Attempts : {attempts}")
             print("Password: " + guess)
             quit()
