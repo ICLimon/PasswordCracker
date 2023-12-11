e67e2ee2e82391cea10b327648ddd6471afefd0c
ICLimon <149421819+ICLimon@users.noreply.github.com>
2023-12-11 09:12:45 -0600

    Update and rename pwCracker to pwCracker.py

--- pwCracker.py
+++ pwCracker.py
@@ -4,7 +4,6 @@ import string
 
 pwGuess = ""
 guess = ""
-attempts = 0
 passwordFile = "passList.txt"
 
 attack = input("Select attack: Dictionary  |  Brute     ")
@@ -12,20 +11,24 @@ mode = input("Select mode: SHA-256  |  MD5  |  Text     ")
 
 password = input("What is the password? ")
 
+# Translates password to MD5 hash
 def md5Password(password):
     encodedMd5Password = password.encode("utf-8")
     return(hashlib.md5(encodedMd5Password.rstrip()).hexdigest())
 
+# Trandlates password to SHA-256 hash
 def shaPassword(password):
     encodedSha256Password = password.encode("utf-8")
     return(hashlib.sha256(encodedSha256Password.rstrip()).hexdigest())
 
+# Gets random word
 def bruteForce():
         guess = ''.join(random.choices(string.ascii_letters + string.digits, k = len(password)))
         return guess
 
 count = 0
 
+# Dictionary attack to according mode and prints attempts
 if(attack == "Dictionary"):
     passwordFile = open(passwordFile)
     for word in passwordFile:
@@ -48,6 +51,7 @@ if(attack == "Dictionary"):
             quit()
     print("Password not found")
 
+# Brute force attack according to mode and prints attempts
 if(attack == "Brute"):
     print("Cracking Password...")
     while True:
@@ -62,13 +66,12 @@ if(attack == "Brute"):
             pwGuess = password.rstrip()
             guess = randomWord.rstrip()
             
-        attempts = attempts + 1
-        print(attempts)
+        count = count + 1
         print("-----" + str(randomWord) + "-----")
         if(attempts > 100000):
             print("Password not found")
             quit()
         if(guess == pwGuess):
-            print(f"Attempts : {attempts}")
+            print(f"Attempts : {count}")
             print("Password: " + guess)
             quit()
